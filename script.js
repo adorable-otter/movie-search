847dcd3c97e5693d228f19eb1936ccb96d411ce1
adorable-otter <plan.stack@gmail.com>
2024-10-15 20:33:30 +0900

    영화 상세정보 모달 추가

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 import { snakeToCamelObj } from './util.js';
 
-document.addEventListener('DOMContentLoaded', () => {
-  showPopMovieList();
-});
-
+const IMG_BASE_URL = 'https://image.tmdb.org/t/p';
 const options = {
   method: 'GET',
   headers: {
@@ -12,23 +9,60 @@ const options = {
   },
 };
 
-const IMG_BASE_URL = 'https://image.tmdb.org/t/p';
+document.addEventListener('DOMContentLoaded', () => {
+  const movieList = document.querySelector('ul');
+  showPopMovieList();
+
+  movieList.addEventListener('click', (e) => {
+    showMovieDetail(e);
+  });
+});
 
 const showPopMovieList = async () => {
   const $cardList = document.querySelector('ul');
-  let response;
-  try {
-    response = await fetch(
-      'https://api.themoviedb.org/3/movie/popular?language=ko&page=1',
-      options
-    );
-  } catch (err) {
+  const response = await fetch(
+    'https://api.themoviedb.org/3/movie/popular?language=ko&page=1',
+    options
+  );
+  if (response.ok) {
+    const results = (await response.json()).results;
+    results?.forEach((result) => {
+      $cardList.appendChild(createMovieCard(snakeToCamelObj(result)));
+    });
+  } else {
     alert('데이터를 가져오는데 실패했습니다.');
   }
-  const json = await response.json();
-  json.results.forEach((result) => {
-    $cardList.appendChild(createMovieCard(snakeToCamelObj(result)));
-  });
+};
+
+const showMovieDetail = async (e) => {
+  const $movieCard = e.target.closest('li');
+  const $modal = document.querySelector('[data-name=movie-detail]');
+  const response = await fetch(
+    `https://api.themoviedb.org/3/movie/${$movieCard.dataset.id}?language=ko`,
+    options
+  );
+  if (response.ok) {
+    const result = await response.json();
+    setModalData(snakeToCamelObj(result));
+    $modal.classList.remove('hidden');
+  } else {
+    alert('데이터를 가져오는데 실패했습니다.');
+  }
+};
+
+const setModalData = ({ posterPath, overview, releaseDate, voteAverage, id }) => {
+  const imgSize = '/w200';
+  const $detailImg = document.querySelector('[data-name=detail-img');
+  const $overview = document.querySelector('[data-name=overview');
+  const $releaseDate = document.querySelector('[data-name=release-date');
+  const $voteAverage = document.querySelector('[data-name=detail-rating');
+  const $selectedMovieId = document.querySelector('[data-selected-movie-id');
+
+  $detailImg.style.backgroundImage = `url(${IMG_BASE_URL + imgSize + posterPath})`;
+  $overview.textContent = overview;
+  $releaseDate.textContent = releaseDate;
+  $voteAverage.textContent = voteAverage;
+  $selectedMovieId.textContent = id;
 };
 
 const createMovieCard = ({ title, id, voteAverage, posterPath }) => {
